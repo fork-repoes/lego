7abf675942e36b39e8aeed3aedc0ee7db42eb555
李涛 <litao@zhisland.com>
2022-07-31 08:53:55 +0800

    refactor

--- support/lego-demo/src/main/java/com/geekhalo/lego/joininmemory/web/OrderDetailServiceV2.java
+++ support/lego-demo/src/main/java/com/geekhalo/lego/joininmemory/web/OrderDetailServiceV2.java
@@ -43,12 +43,21 @@ public class OrderDetailServiceV2 implements OrderDetailService{
     public List<OrderDetailVO> getByUserId(Long userId) {
         List<Order> orders = this.orderRepository.getByUserId(userId);
 
+        List<OrderDetailVO> orderDetailVOS = orders.stream()
+                .map(order -> new OrderDetailVO(OrderVO.apply(order)))
+                .collect(toList());
+
         List<Long> userIds = orders.stream()
                 .map(Order::getUserId)
                 .collect(toList());
         List<User> users = this.userRepository.getByIds(userIds);
         Map<Long, User> userMap = users.stream()
                 .collect(toMap(User::getId, Function.identity(), (a, b) -> a));
+        for (OrderDetailVO orderDetailVO : orderDetailVOS){
+            User user = userMap.get(orderDetailVO.getOrder().getUserId());
+            UserVO userVO = UserVO.apply(user);
+            orderDetailVO.setUser(userVO);
+        }
 
         List<Long> addressIds = orders.stream()
                 .map(Order::getAddressId)
@@ -56,6 +65,11 @@ public class OrderDetailServiceV2 implements OrderDetailService{
         List<Address> addresses = this.addressRepository.getByIds(addressIds);
         Map<Long, Address> addressMap = addresses.stream()
                 .collect(toMap(Address::getId, Function.identity(), (a, b) -> a));
+        for (OrderDetailVO orderDetailVO : orderDetailVOS){
+            Address address = addressMap.get(orderDetailVO.getOrder().getAddressId());
+            AddressVO addressVO = AddressVO.apply(address);
+            orderDetailVO.setAddress(addressVO);
+        }
 
         List<Long> productIds = orders.stream()
                 .map(Order::getProductId)
@@ -63,27 +77,12 @@ public class OrderDetailServiceV2 implements OrderDetailService{
         List<Product> products = this.productRepository.getByIds(productIds);
         Map<Long, Product> productMap = products.stream()
                 .collect(toMap(Product::getId, Function.identity(), (a, b) -> a));
-
-        List<OrderDetailVO> orderDetailVOS = Lists.newArrayListWithCapacity(orders.size());
-
-        for (Order order : orders){
-            OrderVO orderVO = OrderVO.apply(order);
-            OrderDetailVO orderDetailVO = new OrderDetailVO(orderVO);
-
-            User user = userMap.get(order.getUserId());
-            UserVO userVO = UserVO.apply(user);
-            orderDetailVO.setUser(userVO);
-
-            Address address = addressMap.get(order.getAddressId());
-            AddressVO addressVO = AddressVO.apply(address);
-            orderDetailVO.setAddress(addressVO);
-
-            Product product = productMap.get(order.getProductId());
+        for (OrderDetailVO orderDetailVO : orderDetailVOS){
+            Product product = productMap.get(orderDetailVO.getOrder().getProductId());
             ProductVO productVO = ProductVO.apply(product);
             orderDetailVO.setProduct(productVO);
-
-            orderDetailVOS.add(orderDetailVO);
         }
+
         return orderDetailVOS;
     }
 }
