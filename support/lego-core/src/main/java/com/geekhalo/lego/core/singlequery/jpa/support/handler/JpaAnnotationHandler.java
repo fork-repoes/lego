c269ca80ef52ab8792a9b4417136768a1dbc71d2
李涛 <litao@zhisland.com>
2022-09-01 08:28:52 +0800

    add

--- support/lego-core/src/main/java/com/geekhalo/lego/core/singlequery/jpa/support/handler/JpaAnnotationHandler.java
+++ support/lego-core/src/main/java/com/geekhalo/lego/core/singlequery/jpa/support/handler/JpaAnnotationHandler.java
@@ -16,7 +16,33 @@ import java.lang.reflect.Field;
  */
 public interface JpaAnnotationHandler<A extends Annotation> extends SmartComponent<Annotation> {
 
+    /**
+     * 是否支持 Annotion
+     * @param annotation
+     * @return
+     */
+    @Override
+    boolean support(Annotation annotation);
+
+    /**
+     * 创建 Predicate
+     * @param root
+     * @param query
+     * @param criteriaBuilder
+     * @param annotation
+     * @param value
+     * @param <E>
+     * @return
+     */
     <E> Predicate create(Root<E> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder, A annotation, Object value);
 
+    /**
+     * 查找 Entity 中的属性，主要用于配置检测
+     * @param entityCls
+     * @param a
+     * @param queryType
+     * @param <E>
+     * @return
+     */
     <E> Field findEntityField(Class<E> entityCls, A a, Class queryType);
 }
