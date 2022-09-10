7569a8339cbeda30bac72c705836e72d90aa5c8f
李涛 <litao@zhisland.com>
2022-09-11 00:31:12 +0800

    change order

--- support/lego-starter/src/main/java/com/geekhalo/lego/starter/validator/ValidatorAutoConfiguration.java
+++ support/lego-starter/src/main/java/com/geekhalo/lego/starter/validator/ValidatorAutoConfiguration.java
@@ -1,20 +1,25 @@
 package com.geekhalo.lego.starter.validator;
 
-import com.geekhalo.lego.annotation.spliter.Split;
 import com.geekhalo.lego.common.validator.ValidateErrorReporter;
 import com.geekhalo.lego.common.validator.Validateable;
-import com.geekhalo.lego.core.spliter.support.spring.SplitInterceptor;
 import com.geekhalo.lego.core.validator.ValidateableBasedValidator;
 import com.geekhalo.lego.core.validator.ValidateableMethodValidationInterceptor;
 import org.springframework.aop.PointcutAdvisor;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.annotation.AnnotationMatchingPointcut;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+
+import java.util.List;
 
 /**
  * Created by taoli on 2022/9/10.
@@ -23,6 +28,7 @@ import org.springframework.validation.annotation.Validated;
  */
 @Configuration
 @ConditionalOnClass(Validateable.class)
+@AutoConfigureAfter(ValidationAutoConfiguration.class)
 public class ValidatorAutoConfiguration {
 
     @Bean
@@ -47,8 +53,17 @@ public class ValidatorAutoConfiguration {
 
     @Bean
     public PointcutAdvisor validateableMethodValidationAdvisor(@Autowired ValidateableMethodValidationInterceptor interceptor){
-        return new DefaultPointcutAdvisor(
+        DefaultPointcutAdvisor pointcutAdvisor = new DefaultPointcutAdvisor(
                 new AnnotationMatchingPointcut(Validated.class, null , true),
                 interceptor);
+        pointcutAdvisor.setOrder(Ordered.LOWEST_PRECEDENCE);
+        return pointcutAdvisor;
     }
+
+    @Autowired
+    public void configMethodValidationOrder(List<MethodValidationPostProcessor> methodValidationPostProcessors){
+        methodValidationPostProcessors.forEach(methodValidationPostProcessor ->
+                methodValidationPostProcessor.setBeforeExistingAdvisors(true));
+    }
+
 }
