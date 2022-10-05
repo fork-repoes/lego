e328c59ad65479e54c8a99407459b14ede7befa6
李涛 <litao@zhisland.com>
2022-10-05 19:11:02 +0800

    opt import

--- support/lego-demo/src/main/java/com/geekhalo/lego/DemoApplication.java
+++ support/lego-demo/src/main/java/com/geekhalo/lego/DemoApplication.java
@@ -1,14 +1,9 @@
 package com.geekhalo.lego;
 
 
-import com.geekhalo.lego.core.command.EnableCommandService;
-import com.geekhalo.lego.core.query.EnableQueryService;
-import com.geekhalo.lego.core.singlequery.jpa.support.JpaBasedQueryObjectRepositoryFactoryBean;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
