91a618978964eb292fd51a671f1062b823620f37
李涛 <litao@zhisland.com>
2022-11-13 13:27:25 +0800

    opt import

--- support/lego-starter/src/main/java/com/geekhalo/lego/starter/feign/FeignRpcConfiguration.java
+++ support/lego-starter/src/main/java/com/geekhalo/lego/starter/feign/FeignRpcConfiguration.java
@@ -1,7 +1,6 @@
 package com.geekhalo.lego.starter.feign;
 
 import com.geekhalo.lego.core.feign.*;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
