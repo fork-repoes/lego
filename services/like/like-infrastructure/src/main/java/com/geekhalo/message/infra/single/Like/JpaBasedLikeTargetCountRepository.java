8125b21faba8d02f2dcaca1d30fc1d1bf71cc055
tao.li <litao851025@163.com>
2023-05-14 23:20:42 +0800

    add repository

--- services/like/like-infrastructure/src/main/java/com/geekhalo/message/infra/single/Like/JpaBasedLikeTargetCountRepository.java
+++ services/like/like-infrastructure/src/main/java/com/geekhalo/message/infra/single/Like/JpaBasedLikeTargetCountRepository.java
@@ -1,7 +1,16 @@
 package com.geekhalo.message.infra.single.Like;
 
+import com.geekhalo.like.domain.ActionTarget;
+import com.geekhalo.like.domain.like.LikeTargetCount;
 import com.geekhalo.like.domain.like.LikeTargetCountRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface JpaBasedLikeTargetCountRepository
-    extends LikeTargetCountRepository {
+    extends LikeTargetCountRepository, JpaRepository<LikeTargetCount, Long> {
+
+    @Modifying
+    @Query("update LikeTargetCount set count = count + ?2 where target = ?1")
+    void incr(ActionTarget target, int count);
 }
--- services/like/like-infrastructure/src/main/java/com/geekhalo/message/infra/single/dislike/JpaBasedDislikeTargetCountRepository.java
+++ services/like/like-infrastructure/src/main/java/com/geekhalo/message/infra/single/dislike/JpaBasedDislikeTargetCountRepository.java
@@ -1,7 +1,16 @@
 package com.geekhalo.message.infra.single.dislike;
 
+import com.geekhalo.like.domain.ActionTarget;
+import com.geekhalo.like.domain.dislike.DislikeTargetCount;
 import com.geekhalo.like.domain.dislike.DislikeTargetCountRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface JpaBasedDislikeTargetCountRepository
-    extends DislikeTargetCountRepository {
+    extends DislikeTargetCountRepository, JpaRepository<DislikeTargetCount, Long> {
+
+    @Modifying
+    @Query("update DislikeTargetCount set count = count + ?2 where target = ?1")
+    void incr(ActionTarget target, int count);
 }
