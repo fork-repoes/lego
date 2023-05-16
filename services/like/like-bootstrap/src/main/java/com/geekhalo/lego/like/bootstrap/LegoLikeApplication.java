7cb6eb991314ae21888beee388107613d9e768a8
tao.li <litao851025@163.com>
2023-05-17 07:01:11 +0800

    add bootstrap

---
+++ services/like/like-bootstrap/src/main/java/com/geekhalo/lego/like/bootstrap/LegoLikeApplication.java
@@ -0,0 +1,11 @@
+package com.geekhalo.lego.like.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoLikeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LegoLikeApplication.class, args);
+    }
+}
