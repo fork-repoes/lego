37e7a214ab6c382b4cb81d23e988950b217e35b3
tao.li <litao851025@163.com>
2023-06-05 07:18:50 +0800

    refactor

--- support/lego-core/src/main/java/com/geekhalo/lego/core/command/support/handler/CommandHandler.java
+++ support/lego-core/src/main/java/com/geekhalo/lego/core/command/support/handler/CommandHandler.java
@@ -1,6 +1,5 @@
 package com.geekhalo.lego.core.command.support.handler;
 
-import com.geekhalo.lego.core.command.Command;
 
 public interface CommandHandler<CMD, RESULT> {
     RESULT handle(CMD cmd);
