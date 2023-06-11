7a677cfddbd64bb4c21a10cbc6f138e9a816b830
tao.li <litao851025@163.com>
2023-06-11 17:14:09 +0800

    refactor

--- services/like/like-feign-service/src/main/java/com/geekhalo/like/feign/service/ActionQueryFeignService.java
+++ services/like/like-feign-service/src/main/java/com/geekhalo/like/feign/service/ActionQueryFeignService.java
@@ -2,10 +2,12 @@ package com.geekhalo.like.feign.service;
 
 import com.geekhalo.like.api.ActionQueryApi;
 import com.geekhalo.like.api.ActionVO;
-import com.geekhalo.like.app.ActionQueryApplicationService;
+import com.geekhalo.like.app.DislikeQueryApplicationService;
+import com.geekhalo.like.app.LikeQueryApplicationService;
 import com.geekhalo.like.domain.AbstractAction;
 import com.geekhalo.like.domain.dislike.DislikeAction;
 import com.geekhalo.like.domain.like.LikeAction;
+import com.geekhalo.like.domain.user.ActionUser;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +22,15 @@ import java.util.stream.Collectors;
 @RequestMapping(ActionQueryApi.PATH)
 public class ActionQueryFeignService implements ActionQueryApi {
     @Autowired
-    private ActionQueryApplicationService actionQueryApplicationService;
+    private LikeQueryApplicationService likeQueryApplicationService;
+
+    @Autowired
+    private DislikeQueryApplicationService dislikeQueryApplicationService;
+
 
     @Override
     public List<ActionVO> getLikeByUserAndType(Long userId, String type) {
-        List<LikeAction> likeActions = this.actionQueryApplicationService.getLikeByUserAndType(userId, type);
+        List<LikeAction> likeActions = this.likeQueryApplicationService.getLikeByUserAndType(ActionUser.apply(userId), type);
         return likeActions.stream()
                 .map(this::toActionVO)
                 .filter(Objects::nonNull)
@@ -34,7 +40,7 @@ public class ActionQueryFeignService implements ActionQueryApi {
 
     @Override
     public List<ActionVO> getDislikeByUserAndType(Long userId, String type) {
-        List<DislikeAction> actions = this.actionQueryApplicationService.getDislikeByUserAndType(userId, type);
+        List<DislikeAction> actions = this.dislikeQueryApplicationService.getDislikeByUserAndType(ActionUser.apply(userId), type);
         return actions.stream()
                 .map(this::toActionVO)
                 .filter(Objects::nonNull)
